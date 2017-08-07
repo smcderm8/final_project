ea8d68da060c89b5c3bc5a06dc5c70fe5e191385
Raghu Betina <raghu@firstdraft.com>
2017-08-07 19:49:37 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "teams#index"
   # Routes for the Tee_time resource:
   # CREATE
