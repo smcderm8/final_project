00a1a0e322b460dce9f4c915d07fc97973a278f1
Raghu Betina <raghu@firstdraft.com>
2017-08-07 19:49:14 +0000

    Generate courses resource with starter:resource

---
+++ db/migrate/20170807194914_create_courses.rb
@@ -0,0 +1,14 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.string :address
+      t.string :par
+      t.string :rating
+      t.string :slope
+
+      t.timestamps
+
+    end
+  end
+end
